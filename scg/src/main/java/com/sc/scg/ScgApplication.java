30d76c61b1da9e196dcaa67aecf8316cefce06eb
ondal <hiondal@gmail.com>
2024-06-12 21:34:30 +0900

    routing rule test

--- scg/src/main/java/com/sc/scg/ScgApplication.java
+++ scg/src/main/java/com/sc/scg/ScgApplication.java
@@ -1,6 +1,7 @@
 package com.sc.scg;
 
 import com.sc.scg.lb.ServiceDiscovery;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,11 +9,15 @@ import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Slf4j
 public class ScgApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(ScgApplication.class, args);
 
+        log.info("************* Get services **************");
         ServiceDiscovery serviceDiscovery = context.getBean(ServiceDiscovery.class);
+
+        log.info("*** MEMBER-SERVICE ***");
         serviceDiscovery.getServiceInstances("member-service");
     }
 }
